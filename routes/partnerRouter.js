ae04b149a68c32d5b3d55a6d708c2df3ffef8100
L2M <loren.milliman@gmail.com>
2022-09-10 13:19:15 -0700

    feat: admin access points, restrict update/delete comment to author

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -17,7 +17,7 @@ partnerRouter.route('/')
   })
   .catch(err => next(err))
 })
-.post(authenticate.verifyUser, (req, res) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
   Partner.create(req.body)
   .then(partner => {
     console.log('partner Created ', partner)
@@ -31,7 +31,7 @@ partnerRouter.route('/')
     res.statusCode = 403
     res.end('PUT operation not supported on /partners')
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Partner.deleteMany()
   .then(response => {
     res.statusCode = 200
@@ -58,7 +58,7 @@ partnerRouter.route('/:partnerId')
   res.statusCode = 403
   res.end(`POST operation not supported on /partners/${req.params.partnerId}`)
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Partner.findByIdAndUpdate(req.params.partnerId, {
     $set: req.body
   }, { new: true })
@@ -69,7 +69,7 @@ partnerRouter.route('/:partnerId')
   })
   .catch(err => next(err))
 })
-.delete(authenticate.verifyUser, (req, res) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
   Partner.findByIdAndDelete(req.params.partnerId)
   .then(respond => {
     res.statusCode = 200
